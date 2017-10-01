07d557c083cd40074074d9df99690657c0917d46
Angelica 1151453 <angelicamariabs@ufps.edu.co>
2017-10-01 14:56:41 -0500

    Frontd end primer previo

---
+++ general.js
@@ -0,0 +1,43 @@
+$(document).ready(function () {
+ var todo ='<div class="row">\n' +
+     '\n' +
+     '                    <div class="card" style="width: 15rem;">\n' +
+     '                        <img class="card-img-top" src="http://lorempixel.com/300/300/food" alt="Card image cap">\n' +
+     '                        <div class="card-body">\n' +
+     '                            <h4 class="card-title">Producto</h4>\n' +
+     '                            <p class="card-text">Some quick example text to build on the card title and make up the bulk\n' +
+     '                                of the card\'s content.</p>\n' +
+     '                            <a href="#" class="btn btn-info">Detalles</a>\n' +
+     '                            <a href="#" class="btn btn-success">Agregar</a>\n' +
+     '                        </div>\n' +
+     '                    </div>\n' +
+     '\n' +
+     '                    <div class="card" style="width: 15rem;">\n' +
+     '                        <img class="card-img-top" src="http://lorempixel.com/300/300/food" alt="Card image cap">\n' +
+     '                        <div class="card-body">\n' +
+     '                            <h4 class="card-title">Producto</h4>\n' +
+     '                            <p class="card-text">Some quick example text to build on the card title and make up the bulk\n' +
+     '                                of the card\'s content.</p>\n' +
+     '                            <a href="#" class="btn btn-info">Detalles</a>\n' +
+     '                            <a href="#" class="btn btn-success">Agregar</a>\n' +
+     '                        </div>\n' +
+     '                    </div>\n' +
+     '\n' +
+     '                    <div class="card" style="width: 15rem;">\n' +
+     '                        <img class="card-img-top" src="http://lorempixel.com/300/300/food" alt="Card image cap">\n' +
+     '                        <div class="card-body">\n' +
+     '                            <h4 class="card-title">Producto</h4>\n' +
+     '                            <p class="card-text">Some quick example text to build on the card title and make up the bulk\n' +
+     '                                of the card\'s content.</p>\n' +
+     '                            <a href="#" class="btn btn-info">Detalles</a>\n' +
+     '                            <a href="#" class="btn btn-success">Agregar</a>\n' +
+     '                        </div>\n' +
+     '                    </div>\n' +
+     '\n' +
+     '                </div>';
+
+    $('#productos-activos').append(todo);
+
+});
+
+
